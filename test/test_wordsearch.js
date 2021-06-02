fded30a5a5f857691e23841b6db1af9bb26f81aa
henriw22 <henriw22@gmail.com>
2021-06-02 23:11:00 +0000

    completed the code to be able to find a word in a matrix horizontally, vertically and backwards

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,6 +3,7 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
+
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
@@ -32,6 +33,21 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['T', 'T', 'A', 'M', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MATT')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,51 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//Pair programming by Matthew Tamkee and Henri William
+
+const transpose = function(matrix) {
+    // Put your solution here
+    let result = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      let tempArray = [];
+      for (let j = 0; j < matrix.length; j++) {
+        tempArray.push(matrix[j][i]);
+      }
+      result.push(tempArray);
     }
+    return result;
+  };
+  
+const find = (matrix, word) => {
+    for (let i of matrix) {
+        if (i.includes(word)) {
+            return true;
+        }
+    }
+};
+
+const reverseString = (matrix) => {
+    let row = [];
+    for (let rows of matrix) {
+        let rev = rows.split('').reverse().join('');
+        row.push(rev);
+    }
+    return row;
+};
+
+const wordSearch = (letters, word) => { 
+    // [['a','b','c']] => ['abc']
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const transposeMatrix = transpose(letters);
+    const verticalJoin = transposeMatrix.map(ls => ls.join(''))
+    let reverseMatrix = reverseString(horizontalJoin);
+    let reverseSearch = find(reverseMatrix, word);
+    let horizontalSearch = find(horizontalJoin, word);
+    let verticalSearch = find(verticalJoin, word);
+    
+    if (horizontalSearch === true || verticalSearch === true || reverseSearch === true) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 module.exports = wordSearch
